a9c4cfcb73e419fdeaefd86c73a67310850564d4
jeancarlozelaya <jz.92@live.com>
2024-11-18 07:36:33 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/Imágenes/Icono.png',
+        // Agrega los archivos que quieras almacenar en caché
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
