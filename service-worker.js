46752f9e11e0def6c20f74ec78fd6cd232a7effe
jeancarlozelaya <jz.92@live.com>
2024-11-18 08:10:36 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,11 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/Imágenes/Icono.png',
-        // Agrega los archivos que quieras almacenar en caché
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}
